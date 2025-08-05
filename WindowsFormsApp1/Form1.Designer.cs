2d61de4c6a5dfc52bb88eadd26e4f996442a87af
CJ3027317 <CJ3027317@LAB02-PC17>
2025-08-05 12:54:40 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,30 @@
+﻿namespace ProjetoProg
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.PictureBox PicLogin;
+        private System.Windows.Forms.Label LblUser;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.TextBox TxbUser;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.Button BtnLogin;
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace ProjetoProg
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PicBackgroundLogin_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.PicLogin = new System.Windows.Forms.PictureBox();
+            this.LblUser = new System.Windows.Forms.Label();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.TxbUser = new System.Windows.Forms.TextBox();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PicLogin
+            // 
+            this.PicLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PicLogin.BackgroundImage")));
+            this.PicLogin.Image = ((System.Drawing.Image)(resources.GetObject("PicLogin.Image")));
+            this.PicLogin.Location = new System.Drawing.Point(1, 0);
+            this.PicLogin.Name = "PicLogin";
+            this.PicLogin.Size = new System.Drawing.Size(696, 401);
+            this.PicLogin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicLogin.TabIndex = 0;
+            this.PicLogin.TabStop = false;
+            this.PicLogin.Click += new System.EventHandler(this.PicLogin_Click);
+            // 
+            // LblUser
+            // 
+            this.LblUser.AutoSize = true;
+            this.LblUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(251)))), ((int)(((byte)(222)))));
+            this.LblUser.Font = new System.Drawing.Font("Noto Sans", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUser.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LblUser.Location = new System.Drawing.Point(272, 218);
+            this.LblUser.Name = "LblUser";
+            this.LblUser.Size = new System.Drawing.Size(52, 16);
+            this.LblUser.TabIndex = 1;
+            this.LblUser.Text = "Usuário:";
+            this.LblUser.Click += new System.EventHandler(this.LblUser_Click);
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(251)))), ((int)(((byte)(222)))));
+            this.LblPassword.Font = new System.Drawing.Font("Noto Sans", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPassword.Location = new System.Drawing.Point(272, 252);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(44, 16);
+            this.LblPassword.TabIndex = 2;
+            this.LblPassword.Text = "Senha:";
+            this.LblPassword.Click += new System.EventHandler(this.LblPassword_Click);
+            // 
+            // TxbUser
+            // 
+            this.TxbUser.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.TxbUser.Location = new System.Drawing.Point(274, 232);
+            this.TxbUser.Multiline = true;
+            this.TxbUser.Name = "TxbUser";
+            this.TxbUser.Size = new System.Drawing.Size(135, 19);
+            this.TxbUser.TabIndex = 3;
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.TxtPassword.Location = new System.Drawing.Point(275, 267);
+            this.TxtPassword.Multiline = true;
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '•';
+            this.TxtPassword.Size = new System.Drawing.Size(135, 19);
+            this.TxtPassword.TabIndex = 4;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.BtnLogin.Font = new System.Drawing.Font("Noto Sans", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogin.Location = new System.Drawing.Point(313, 304);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(80, 26);
+            this.BtnLogin.TabIndex = 5;
+            this.BtnLogin.Text = "Entrar";
+            this.BtnLogin.UseVisualStyleBackColor = false;
+            this.BtnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // FrmLogin
+            // 
+            this.ClientSize = new System.Drawing.Size(697, 402);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxbUser);
+            this.Controls.Add(this.LblPassword);
+            this.Controls.Add(this.LblUser);
+            this.Controls.Add(this.PicLogin);
+            this.Name = "FrmLogin";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void LblUser_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            FrmDisciplinas  disciplinas = new FrmDisciplinas();
+
+            disciplinas.ShowDialog();
+            this.Visible = true;
+        }
+
+        private void PicLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
